d41a49dc213929a80be484cc887548ded79b8f92
Jack Stubblefield <jack011601@gmail.com>
2022-10-10 14:02:04 -0500

    created BST

--- javascript/401-javascript/trees/tree.js
+++ javascript/401-javascript/trees/tree.js
@@ -71,6 +71,34 @@ class BinaryTree {
     }
 }
 
+class BST extends BinaryTree {
+    constructor() {
+        super(root);
+        this.root = null;
+    }
+
+    insert(value) {
+
+        let newNode = new Node(value);
+        if (this.root === null) { this.root = newNode; }
+        else { this.insertNode(this.root, newNode); }
+    };
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) { node.left = newNode; }
+            else { this.insertNode(node.left, newNode); }
+        }
+
+        else {
+            if (node.right === null) { node.right = newNode; }
+            else { this.insertNode(node.right, newNode); }
+        }
+    }
+};
+
+
+
 
 
 
