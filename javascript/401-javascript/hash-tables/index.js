f0025bcf2b2d224059e89c9ff9f5673b931c548d
Jack Stubblefield <Jack011601@gmail.com>
2022-11-02 17:31:06 -0500

    linked list working inside of hashtable

--- javascript/401-javascript/hash-tables/index.js
+++ javascript/401-javascript/hash-tables/index.js
@@ -10,6 +10,11 @@ class HashTable {
         this.buckets = new Array(size)
     }
 
+    /**
+     * 
+     * @param {String} key what you are hashing
+     * @returns a number
+     */
     hash(key) {
         let characters = key.split('')
         let asciiSum = characters.reduce((sum, character) => {
@@ -17,11 +22,36 @@ class HashTable {
         }, 0)
 
         let initialHash = asciiSum * 599
-        return initialHash % size
+        return initialHash % this.size
+    }
+
+    /**
+     * 
+     * @param {string} key 
+     * @param {*} value 
+     */
+    set(key, value) {
+        let position = this.hash(key)
+        let data = { [key]: value }
+
+
+        if (this.buckets[position]) {
+            let bucket = this.buckets[position]
+            bucket.insert(data)
+
+        } else {
+            let bucket = new LinkedList();
+
+            bucket.insert(data);
+
+            this.buckets[position] = bucket
+        }
     }
 
 }
 
-let table = new HashTable(1024)
+let table = new HashTable(1024);
 console.log(table)
 console.log(table.hash('Jack'));
+table.set('Jack', { name: 'Jack', age: 48 })
+console.log(JSON.stringify(table.buckets[543]));
